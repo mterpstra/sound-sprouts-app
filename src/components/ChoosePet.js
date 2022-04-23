3fea740e4339bb427bf30d475b58014eeef7cf2a
Mark Terpstra <mark.terpstra.mt@gmail.com>
2022-04-23 15:50:25 -0400

    Add text to the choose pet page

--- src/components/ChoosePet.js
+++ src/components/ChoosePet.js
@@ -1,20 +1,18 @@
-import React, { Component, useState, useEffect } from "react";
-import { Text, View, Pressable, Image, ImageBackground, Button, StyleSheet, TouchableOpacity, Platform } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react"; 
+import { Text, View, Image, ImageBackground, StyleSheet, Platform, Dimensions } from 'react-native';
 import SpeechBubble from "./SpeechBubble"
-import { TopWithDede, Layout, Top, Bottom } from "./Layout"
+import { TopWithDede, Top, Bottom } from "./Layout"
 import { COLORS } from '../values/colors.js';
 import MyBox from "./Box"
+import { useFonts } from 'expo-font';
 
 const petshop = require('../../images/background.png');
-
 const dog = require('../../images/dog.png');
 const cat = require('../../images/cat.png');
 const rabbit = require('../../images/rabbit.png');
 const turtle = require('../../images/turtle.png');
 const parrot = require('../../images/parrot.png');
 const hamster = require('../../images/hamster.png');
-
 const message = [
    "We have lots of nice pets to choose from here at the pet shop!",
    "Pick one and get started." 
@@ -22,7 +20,33 @@ const message = [
 
 const BLUR_RADIUS = Platform.OS == 'ios' ? 20 : 4;
 
+const width = Dimensions.get('window').width;
+const FONT_SIZE=(width > 500) ? 60 : 30;
+
+
+const Pet = (props) => {
+   return (
+      <View style={styles.box}>
+         <MyBox 
+            //onPress={() => props.navigation.navigate('NamePet') } 
+         >
+            <Image source={props.source} style={styles.img} />
+         </MyBox>
+         <Text style={styles.text}>{props.name}</Text>
+      </View>
+   );
+}
+
 const ChoosePet = (props) => {
+
+   let [fontsLoaded] = useFonts({
+      'Patrick-Hand': require('../../assets/fonts/PatrickHand-Regular.ttf'),
+   });
+
+   if (!fontsLoaded) {
+      return <View/>;
+   }
+
 
    return (
       <ImageBackground 
@@ -45,54 +69,30 @@ const ChoosePet = (props) => {
                {/* Row 1 */}
                <View style={styles.row}>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={dog} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={dog} name="Dog"/> 
                   </View>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={cat} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={cat} name="Cat"/> 
                   </View>
                </View>
 
                {/* Row 2 */}
                <View style={styles.row}>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={rabbit} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={rabbit} name="Rabbit"/> 
                   </View>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={turtle} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={turtle} name="Turtle"/> 
                   </View>
                </View>
 
                {/* Row 3 */}
                <View style={styles.row}>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={parrot} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={parrot} name="Parrot"/> 
                   </View>
                   <View style={styles.col}>
-                     <View style={styles.box}>
-                        <MyBox>
-                           <Image source={hamster} style={styles.img} />
-                        </MyBox>
-                     </View>
+                     <Pet source={hamster} name="Hamster"/> 
                   </View>
                </View>
 
@@ -113,7 +113,7 @@ const styles = StyleSheet.create({
    },
    box: {
       flex:1,
-      flexDirection:"row",
+      flexDirection:"column",
       alignItems: "stretch",
       margin:20,
    },
@@ -122,6 +122,14 @@ const styles = StyleSheet.create({
       width: undefined,
       height: undefined,
       resizeMode:'contain',
+   },
+   text: {
+      fontSize: FONT_SIZE,
+      lineHeight: FONT_SIZE+5,
+      color:COLORS.red,
+      textAlign:"center",
+      fontFamily: 'Patrick-Hand',
+      flexWrap: 'wrap',
    },
 });
 
